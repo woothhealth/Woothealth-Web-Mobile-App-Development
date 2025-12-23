9bd0a34778cdfeed39b077d071c2a1ac2969e03e
Dahvid <dahvidweb@gmail.com>
2025-12-23 05:49:49 -0800

    Hero change

--- Components/ProvidersPage/HeroSection.jsx
+++ Components/ProvidersPage/HeroSection.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <section className='flex items-center flex-col py-6 text-[#120052] text-center space-y-2 px-4'>
+      <h2 className='text-3xl md:text-[40px] font-bold'>Find Quality Healthcare Near You</h2>
+      <p className='md:w-[55rem] md:text-lg'>Search thousands of trusted hospitals, Pharmacy, Dental Clinics, Diagnostic Centers and Wellness &Therapy Facilities across Nigeria.</p>
+    </section>
   )
 }
 

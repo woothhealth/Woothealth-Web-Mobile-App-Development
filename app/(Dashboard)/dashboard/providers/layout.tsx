5afbedf7882f13a60d2ea0a3e7722ed58507915c
Dahvid <dahvidweb@gmail.com>
2025-12-31 02:08:21 -0800

    Bug fix

--- app/(Dashboard)/dashboard/providers/layout.tsx
+++ app/(Dashboard)/dashboard/providers/layout.tsx
@@ -10,18 +10,15 @@ export const metadata: Metadata = {
 export default function ProvidersLayout({
   children,
   header,
-  transaction,
 }: Readonly<{
   children: React.ReactNode;
   header: React.ReactNode;
-  transaction: React.ReactNode;
 }>) {
   return (
     <>
     <div className="container w-full bg-[#FAFAFA] hidden lg:block">
       <div>{header}</div>
       <div className="px-4">{children}</div>
-      <div className="px-4">{transaction}</div>
     </div>
     </>
   );
